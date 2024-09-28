ba4b5002e9f0f914a9d243ef12823c1ed7e510cb
MrHyaena <dolezalmartin131@gmail.com>
2024-09-28 23:05:18 +0200

    Sixth excercise all tests passed
    I just used else if and conditions in correct
    order

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
 const leapYears = function() {
 
+let year = arguments[0];
+
+
+
+if (year % 400 == 0) {
+return true
+} else if (year % 4 == 0 && year % 100 != 0) {
+    return true
+} else {
+    return false
+}
+
 };
 
 // Do not edit below this line
