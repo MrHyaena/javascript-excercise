1d2dc808cab28b19922ba50d58af2f8b279ef5ce
MrHyaena <dolezalmartin131@gmail.com>
2024-09-28 22:44:24 +0200

    Fifth excercise all test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
 const sumAll = function() {
 
+    if (arguments[0] < 0 || arguments[1] < 0 || arguments[0] % 1 > 0 || arguments[1] % 1 > 0 || typeof arguments[0] != "number" || typeof arguments[1] != "number") {
+        return "ERROR"
+    } else {
+
+
+
+highestNumber = Math.max(arguments[0], arguments[1])
+lowestNumber = Math.min(arguments[0], arguments[1])
+sum = 0
+
+
+
+for (i = 0; i < highestNumber; i++) {
+
+    sum = sum + (i + 1)
+}
+
+return sum
+
+
+}
 };
 
 // Do not edit below this line
