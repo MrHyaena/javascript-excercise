42669b6b452971437219c9f59ec4e038bc53a2e8
MrHyaena <dolezalmartin131@gmail.com>
2024-09-29 14:47:51 +0200

    Seventh excercise all tests passed
    I just used simple conversion formula and
    math.round function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
 const convertToCelsius = function() {
+
+  let fahr = arguments[0]
+
+  let numC = (fahr - 32) * 5 / 9
+
+  let rnumC = Math.round(numC * 10) / 10
+
+  return rnumC
+
 };
 
 const convertToFahrenheit = function() {
+
+  let cels = arguments[0]
+
+  let numF = (cels * 9 / 5) + 32
+
+  let rnumF = Math.round(numF * 10) / 10
+
+  return rnumF
+
 };
 
 // Do not edit below this line
