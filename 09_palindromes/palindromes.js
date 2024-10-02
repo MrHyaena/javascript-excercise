af8f5edcb4cc4c8b2f401fc0b8a55955af903130
MrHyaena <dolezalmartin131@gmail.com>
2024-10-02 22:20:13 +0200

    Excercise 9, all tests passed
    I had a mistake in split syntax

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let array = word.split("");
+    let reverseArray = array.reverse();
 
+    let reverseWord = reverseArray.join("");
+
+    let lowercaseWord = reverseWord.toLowerCase()
+    let lowercaseReverseWord = word.toLowerCase()
+
+    let finalWord = removeFunction(lowercaseWord);
+    let finalReverseWord = removeFunction(lowercaseReverseWord);
+
+    let valueWord = finalWord.valueOf();
+    let valueReverseWord = finalReverseWord.valueOf();
+
+    if (valueWord === valueReverseWord) {
+        return true;
+    } else {
+        return false;
+    }
+
+    function removeFunction(inputString) {
+        return inputString.replace(/[^a-zA-Z0-9]/g, '');
+    }
+    
 };
 
 // Do not edit below this line
