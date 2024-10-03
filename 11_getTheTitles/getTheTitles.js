a4000443ffe4b2db4fac4ce40da73b429efe3522
MrHyaena <dolezalmartin131@gmail.com>
2024-10-03 20:06:53 +0200

    excercise 11 test passed
    I used function map, that runs function over every element and creates
    a new array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
 const getTheTitles = function() {
 
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
+
+    function getTitle(books) {
+        return books.title;
+      }
+      let titles = books.map(getTitle);
+      return titles;
 };
 
 // Do not edit below this line
