98d4294281ad00f577ee355c609f4cba126ee23b
MrHyaena <dolezalmartin131@gmail.com>
2024-10-03 19:26:06 +0200

    Excercise 10, all tests passed
    I used splice method and for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(b) {
 
+    let array = [0, 1]
+    let a = parseInt(b);
+
+    if (a < 0) {
+        return "OOPS";
+    } else {
+
+    for (let i = 2; i < a + 2; i++) {
+        array.splice(i, 0, array[i - 2] + array[i - 1])
+    }
+
+    return array[b];
+}
 };
 
 // Do not edit below this line
