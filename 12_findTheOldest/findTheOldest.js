fa68caad41ce69e49dd56776bc9a5927c62f7f53
MrHyaena <dolezalmartin131@gmail.com>
2024-10-03 23:01:11 +0200

    Excercise 12 I passed the first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(people) {
+
+    
+    function getTheAges(person) {
+        return age = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    let ages = people.map(getTheAges);
+    let oldestAge = Math.max.apply(Math, ages);
+    let indexOldestAge = ages.indexOf(oldestAge);
+    
+    return people[indexOldestAge];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
