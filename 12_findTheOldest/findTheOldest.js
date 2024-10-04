5570a4f53eec28ef1b3c227add69d611f1089244
MrHyaena <dolezalmartin131@gmail.com>
2024-10-04 18:11:54 +0200

    I solved the 12th excercise
    For second and third test I used simple if
    death date is undefined then get the current year
    with function Date

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,8 +3,16 @@ const findTheOldest = function(people) {
 
     
     function getTheAges(person) {
+        if (person.yearOfDeath === undefined) {
+            let currentDate = new Date();
+            let currentYear = currentDate.getFullYear();
+
+            return age = currentYear - person.yearOfBirth;
+        } else {
+
         return age = person.yearOfDeath - person.yearOfBirth;
     }
+    };
 
     let ages = people.map(getTheAges);
     let oldestAge = Math.max.apply(Math, ages);
